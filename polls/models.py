a7a8e825b8f44a9f380e677d4c006337d798b4f8
Manpreet <go4sunny@gmail.com>
2020-02-01 21:35:43 +0530

    updated based on tutorial part 7

--- polls/models.py
+++ polls/models.py
@@ -13,9 +13,15 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
+
         now = timezone.now()
+
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    was_published_recently.admin_oreder_field = "pub_date"
+    was_published_recently.boolean = True
+    was_published_recently.short_description = "Published recently?"
+    # return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
